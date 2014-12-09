9d8954c670a0db2d8d4b087bff07a06e1f1518b6
Vineet Pedaballe <Vineet.pedaballe@intel.com>
2014-12-08 23:57:16 -0800

    Added capability to individually set the I2C Expander Ports

--- inc/i2cio.h
+++ inc/i2cio.h
@@ -2,8 +2,13 @@
 #define _I2CIO_H_
 
 #include <util.h>
+#include <stdbool.h>
+
+static unsigned char i2c_gpio_out[8]={0x01,0x02,0x04,0x08, 0x10,0x20,0x40,0x80};
 
 void set_io(unsigned char out);
+unsigned char get_io();
+void set_gpio(char gpio, bool gpio_state);
 void i2c_exec(char **argv, int argc);
 #endif
 
--- src/i2cio.c
+++ src/i2cio.c
@@ -1,6 +1,7 @@
 #include <util.h>
 #include "i2cio.h"
 #include <mraa.h>
+#include <stdbool.h>
 
 mraa_i2c_context i2c;
 #define I2C_IO_EXPANDER_ADDR 0x20
@@ -8,7 +9,6 @@ mraa_i2c_context i2c;
 void set_io(unsigned char out)
 {
   int ret = 0;
-  char buf[5];
   i2c = mraa_i2c_init(6);
   mraa_i2c_address(i2c, I2C_IO_EXPANDER_ADDR);
 
@@ -19,9 +19,39 @@ void set_io(unsigned char out)
 
 }
 
+unsigned char get_io()
+{
+  i2c = mraa_i2c_init(6);
+  mraa_i2c_address(i2c,I2C_IO_EXPANDER_ADDR);
+  return mraa_i2c_read_byte(i2c);
+
+}
+
+void set_gpio(char gpio, bool gpio_state)
+{
+  int ret = 0;
+  unsigned char cur_pinout;
+  i2c = mraa_i2c_init(6);
+  mraa_i2c_address(i2c,I2C_IO_EXPANDER_ADDR);
+  cur_pinout = get_io();
+
+  if(gpio_state)
+  {
+    cur_pinout = cur_pinout | i2c_gpio_out[(unsigned int)gpio];
+  } else {
+    cur_pinout = cur_pinout & (i2c_gpio_out[(unsigned int)gpio]^0xff);
+  }
+  
+  if(MRAA_SUCCESS != (ret=mraa_i2c_write_byte(i2c,cur_pinout)))
+  {
+    printf("i2c set error: %x\n",ret);
+  }
+
+}
 void i2c_exec(char **argv, int argc)
 {
   int input_data;
+  bool gpio_state;
 
   printf("argc: %d\n", argc);
 
@@ -36,8 +66,25 @@ void i2c_exec(char **argv, int argc)
       set_io(input_data&0xff);
     }
   } else if (!strcmp(*argv, "r")) {
-    printf("io get command : %s\n", *argv);
-  } else {
+    printf("io read : 0x%x\n", get_io());
+    
+  } else if (!strcmp(*argv, "gw")) {
+    printf("GPIO Bit manipulation");
+    input_data = strtol(*(argv+1),NULL,0);
+    if(!(input_data > 0 && input_data <=7)) {
+      printf("Invalid GPIO bit location\n");
+    } else {
+      if(strtol(*(argv+2),NULL,0))
+        gpio_state = true;
+      else
+        gpio_state = false;
+      set_gpio(input_data, gpio_state);
+    }
+  } else if (!strcmp(*argv, "help")) {
+    printf("I2C IO Expander Pin manipulations\n");
+    printf("bin/prog i2cio <r|w|gw|help> [NA|value to be written|GPIO to be updated] \n");
+    printf("                             [NA|NA|Value to be set for the gpio]\n");
+  }else {
     printf("Unknown command\n");
   }
 }
